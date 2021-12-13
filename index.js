adb521391eb38561a54985653a2bd060161f923a
CoderBig <49120922+dogukanc760@users.noreply.github.com>
2021-12-13 18:25:22 +0300

    update
    update

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const productRoute = require("./routes/product");
 const orderRoute = require("./routes/order");
 const cartRoute = require("./routes/cart");
 const staffRoute = require("./routes/staff");
+const serviceRoute = require("./routes/service");
+var cors = require("cors");
 
 dotenv.config();
 mongoose
@@ -17,14 +19,16 @@ mongoose
     console.log(err);
   });
 
+  app.use(cors());
   app.use(express.json());
+  app.options('*', cors());
   app.use("/api/product", productRoute);
  app.use("/api/user", userRoute);
  app.use("/api/auth", authRoute);
  app.use("/api/order", orderRoute);
  app.use("/api/cart", cartRoute);
  app.use("/api/staff", staffRoute);
-
+ app.use("/api/service", serviceRoute);
 
 
 app.listen(process.env.PORT || 5000, () => {
